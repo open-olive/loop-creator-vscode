868fb47e5af445efe8e50dfe8352fd78d73932f9
Ulric Esparra <ulric.esparra@oliveai.com>
2021-07-08 15:29:27 -0600

    [HELPS-772] Add configs and depdendencies for ESLint, Prettier, and Jest; apply output of ESLint and Prettier to initial template; and fix some problems around TS vs JS and broken logic

--- src/loopCreator.ts
+++ src/loopCreator.ts
@@ -1,14 +1,19 @@
-import * as vscode from "vscode";
-import * as fs from "fs-extra";
-import * as path from "path";
-import { Uri } from "vscode";
-import * as Sqrl from "squirrelly";
-import { openDialogForFolder } from "./utils";
-import createLoopFormHtml from "./createLoopForm.html";
-import readmeTemplate from "./templates/README.md.squirrelly";
-import packageJsonTemplate from "./templates/package.json.squirrelly";
-import indexTemplate from "./templates/src/index.ts.squirrelly";
-import tsconfigTemplate from "./templates/tsconfig.json.squirrelly";
+import * as vscode from 'vscode';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import { Uri } from 'vscode';
+import * as Sqrl from 'squirrelly';
+import { openDialogForFolder } from './utils';
+import createLoopFormHtml from './createLoopForm.html';
+import eslintrcTemplate from './templates/.eslintrc.squirrelly';
+import gitignoreTemplate from './templates/.gitignore.squirrelly';
+import prettierrcTemplate from './templates/.prettierrc.squirrelly';
+import packageJsonTemplate from './templates/package.json.squirrelly';
+import readmeTemplate from './templates/README.md.squirrelly';
+import tsconfigTemplate from './templates/tsconfig.json.squirrelly';
+import indexTemplate from './templates/src/index.ts.squirrelly';
+import indexTestTemplate from './templates/src/index.test.ts.squirrelly';
+import jestGlobalSetupTemplate from './templates/src/jestGlobalSetup.js.squirrelly';
 
 interface IWebviewMessage<T> {
   command: string;
@@ -37,15 +42,15 @@ export class LoopCreator {
   ) {
     let fileContents: string;
 
-    fileContents = Sqrl.render(readmeTemplate, { projectName });
-    await fs.writeFile(path.join(basePath, "README.md"), fileContents);
-
-    fileContents = Sqrl.render(packageJsonTemplate, {
+    fileContents = Sqrl.render(indexTestTemplate, {
       isTypeScript,
       projectName,
       aptitudes,
     });
-    await fs.writeFile(path.join(basePath, "package.json"), fileContents);
+    await fs.writeFile(
+      path.join(basePath, 'src', `index.test.${isTypeScript ? 'ts' : 'js'}`),
+      fileContents
+    );
 
     fileContents = Sqrl.render(indexTemplate, {
       isTypeScript,
@@ -53,15 +58,62 @@ export class LoopCreator {
       aptitudes,
     });
     await fs.writeFile(
-      path.join(basePath, "src", `index.${isTypeScript ? "ts" : "js"}`),
+      path.join(basePath, 'src', `index.${isTypeScript ? 'ts' : 'js'}`),
       fileContents
     );
 
+    fileContents = Sqrl.render(jestGlobalSetupTemplate, {
+      isTypeScript,
+      projectName,
+      aptitudes,
+    });
+    await fs.writeFile(
+      path.join(basePath, 'src', 'jestGlobalSetup.js'),
+      fileContents
+    );
+
+    fileContents = Sqrl.render(eslintrcTemplate, {
+      isTypeScript,
+      projectName,
+      aptitudes,
+    });
+    await fs.writeFile(path.join(basePath, '.eslintrc'), fileContents);
+
+    fileContents = Sqrl.render(gitignoreTemplate, {
+      isTypeScript,
+      projectName,
+      aptitudes,
+    });
+    await fs.writeFile(path.join(basePath, '.gitignore'), fileContents);
+
+    fileContents = Sqrl.render(prettierrcTemplate, {
+      isTypeScript,
+      projectName,
+      aptitudes,
+    });
+    await fs.writeFile(path.join(basePath, '.prettierrc'), fileContents);
+
+    fileContents = Sqrl.render(packageJsonTemplate, {
+      isTypeScript,
+      projectName,
+      aptitudes,
+    });
+    await fs.writeFile(path.join(basePath, 'package.json'), fileContents);
+
+    fileContents = Sqrl.render(readmeTemplate, {
+      isTypeScript,
+      projectName,
+      aptitudes,
+    });
+    await fs.writeFile(path.join(basePath, 'README.md'), fileContents);
+
     if (isTypeScript) {
-      await fs.writeFile(
-        path.join(basePath, "tsconfig.json"),
-        tsconfigTemplate
-      );
+      fileContents = Sqrl.render(tsconfigTemplate, {
+        isTypeScript,
+        projectName,
+        aptitudes,
+      });
+      await fs.writeFile(path.join(basePath, 'tsconfig.json'), fileContents);
     }
   }
 
@@ -72,25 +124,25 @@ export class LoopCreator {
     aptitudes,
   }: LoopFormData) {
     await fs.ensureDir(basePath);
-    await fs.ensureDir(path.join(basePath, "src"));
+    await fs.ensureDir(path.join(basePath, 'src'));
 
     await this.createFiles(
-      language === "TypeScript",
+      language === 'TypeScript',
       basePath,
       projectName,
       aptitudes
     );
 
     let uri = Uri.file(basePath);
-    await vscode.commands.executeCommand("vscode.openFolder", uri, {
+    await vscode.commands.executeCommand('vscode.openFolder', uri, {
       forceNewWindow: true,
     });
   }
 
   async openCreateLoopWebview() {
     const panel = vscode.window.createWebviewPanel(
-      "loopInfo", // Identifies the type of the webview. Used internally
-      "Create Loop", // Title of the panel displayed to the user
+      'loopInfo', // Identifies the type of the webview. Used internally
+      'Create Loop', // Title of the panel displayed to the user
       vscode.ViewColumn.One,
       {
         enableScripts: true,
@@ -102,13 +154,13 @@ export class LoopCreator {
     panel.webview.onDidReceiveMessage(
       async (message: IWebviewMessage<LoopFormData>) => {
         try {
-          if (message.command === "openFolderDialog") {
+          if (message.command === 'openFolderDialog') {
             const uri = await openDialogForFolder();
             panel.webview.postMessage({
-              command: "getProjectPath",
+              command: 'getProjectPath',
               payload: uri.path,
             });
-          } else if (message.command === "createLoop") {
+          } else if (message.command === 'createLoop') {
             await this.createLoop(message.payload);
             panel.dispose();
           } else {
