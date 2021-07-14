c834bcdc3a5ca2af80a28a68c054de68025000ad
Ulric Esparra <ulric.esparra@oliveai.com>
2021-07-14 15:56:59 -0600

    [HELPS-769] Write functional tests for a Typescript loop

---
+++ src/templates/wip/src/whispers/WindowWhisper.ts
@@ -0,0 +1,59 @@
+import { whisper, window } from '@oliveai/ldk';
+
+interface Props {
+  activeWindow: window.WindowInfo;
+}
+export default class WindowWhisper {
+  whisper: whisper.Whisper;
+
+  label = 'Active Window Changed';
+
+  props: Props = {
+    activeWindow: null,
+  };
+
+  constructor(activeWindow: window.WindowInfo) {
+    this.props.activeWindow = activeWindow;
+  }
+
+  createComponents() {
+    const name: whisper.ListPair = {
+      type: whisper.WhisperComponentType.ListPair,
+      copyable: true,
+      label: 'Window Name',
+      value: this.props.activeWindow.path,
+      style: whisper.Urgency.None,
+    };
+    const pid: whisper.ListPair = {
+      type: whisper.WhisperComponentType.ListPair,
+      copyable: true,
+      label: 'Process Id',
+      value: this.props.activeWindow.pid.toString(),
+      style: whisper.Urgency.None,
+    };
+    return [name, pid];
+  }
+
+  show() {
+    whisper
+      .create({
+        components: this.createComponents(),
+        label: this.label,
+        onClose: WindowWhisper.onClose,
+      })
+      .then((newWhisper) => {
+        this.whisper = newWhisper;
+      });
+  }
+
+  close() {
+    this.whisper.close(WindowWhisper.onClose);
+  }
+
+  static onClose(err?: Error) {
+    if (err) {
+      console.error('There was an error closing Window whisper', err);
+    }
+    console.log('Window whisper closed');
+  }
+}
