68a7775301c057d9526f0fd514e64aa4b6868765
AzureAD\MichaelMcCarrick <michael.mccarrick@oliveai.com>
2022-01-12 10:56:28 -0500

    Added process aptitude to vscode loop templates

--- loop-templates/templates/src/aptitudes/index.ts
+++ loop-templates/templates/src/aptitudes/index.ts
@@ -1,20 +1,22 @@
-import clipboard from './clipboard';
-import filesystem from './filesystem';
-import keyboard from './keyboard';
-import network from './network';
-import ui from './ui';
-import window from './window';
-import index from './index.ts.squirrelly';
-import { FileMap } from '@/types';
+import clipboard from "./clipboard";
+import filesystem from "./filesystem";
+import keyboard from "./keyboard";
+import network from "./network";
+import process from "./process";
+import ui from "./ui";
+import window from "./window";
+import index from "./index.ts.squirrelly";
+import { FileMap } from "@/types";
 
 const fileMap: FileMap = {
-  clipboard: { fileName: 'clipboard', aptitude: 'clipboard' },
-  filesystem: { fileName: 'filesystem', aptitude: 'filesystem' },
-  keyboard: { fileName: 'keyboard', aptitude: 'keyboard' },
-  network: { fileName: 'network', aptitude: 'network' },
-  ui: { fileName: 'ui', aptitude: 'nonzero' },
-  window: { fileName: 'window', aptitude: 'window' },
-  index: { fileName: 'index.ts', aptitude: 'any' }
+  clipboard: { fileName: "clipboard", aptitude: "clipboard" },
+  filesystem: { fileName: "filesystem", aptitude: "filesystem" },
+  keyboard: { fileName: "keyboard", aptitude: "keyboard" },
+  network: { fileName: "network", aptitude: "network" },
+  process: { fileName: "process", aptitude: "process" },
+  ui: { fileName: "ui", aptitude: "nonzero" },
+  window: { fileName: "window", aptitude: "window" },
+  index: { fileName: "index.ts", aptitude: "any" },
 };
 
 export default {
@@ -22,8 +24,9 @@ export default {
   filesystem,
   keyboard,
   network,
+  process,
   ui,
   window,
   index,
-  fileMap
+  fileMap,
 };
